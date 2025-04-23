930f8f74e77f3df7530aff782172ed3038fc3abf
NguyenQuocKhai0505 <nguyenquockhai05.contact@gmail.com>
2025-04-23 23:00:49 +0700

    Header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,56 @@
+import logo from "../../assets/images/logo.png"
+import { Link } from 'react-router-dom';
+import React from 'react';
+import CountryDropDown from "../ContryDropDown";
+import { IoIosSearch, IoMdSearch } from "react-icons/io";
+import Button from '@mui/material/Button';
+import { FaUserCircle } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa6";
+
+
+const Header =()=>
+{
+    return(
+        <>
+        <div className="headerWrapper">
+            <div className="top-strip bg-blue">
+                <div className="container">
+                    <p className="m-0 mt-0 text-center">Discover a World of Quality Shopping – Only the Best for You!</p>
+                </div>
+            </div>
+            
+            <header className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="logoWrapper d-flex align-items-center col-sm-2">
+                            <Link to={'/'}><img src={logo} alt='Logo'/></Link>
+                        </div>
+                        <div className="col-sm-10 d-flex align-items-center part2">
+                            <CountryDropDown/>
+
+                            {/*Header Search Start Here*/}
+                            <div className="headerSearch ml-3 mr-3">
+                                <input type="text" placeholder="Searching for Products...."/>
+                                <Button><IoMdSearch/></Button>
+                            </div>
+
+                            {/*Header Search End Here*/}
+
+
+                            <div className="part3 d-flex align-items-center ml-auto">
+                                <Button className="circle mr-3"><FaUserCircle/></Button>
+                                <div className="ml-auto cartTab">
+                                    <span className="price">$3.29</span>
+                                    <Button className="circle ml-2"><FaCartPlus/></Button>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+        </>
+    )
+}
+export default Header
